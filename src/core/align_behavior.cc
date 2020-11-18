60af3d9d8834a4b063e3544a0dbbdcb4f7359ade
Ravi Patel <ravipatel1713@gmail.com>
2020-11-18 00:25:32 -0600

    Added code for Alignment behavior for flock of boids.

--- src/core/align_behavior.cc
+++ src/core/align_behavior.cc
@@ -1,5 +1,5 @@
 #include "core/align_behavior.h"
-
+#include "core/boid_flocking_config.h"
 namespace boids_flocking{
 
 AlignmentBehavior::AlignmentBehavior(bool align_bahavior_enabled)
@@ -7,7 +7,21 @@ AlignmentBehavior::AlignmentBehavior(bool align_bahavior_enabled)
 
 vec2 AlignmentBehavior::CalculateAlignmentAdjustment(const std::vector<Boid> &flock, const Boid &boid) {
   //TODO: Add code to calculate the vector to align a boid to its neighborhood
-
-  return glm::vec2();
+  vec2 alignment_offset_vector = vec2(0, 0);
+  for(const Boid &current_boid : flock){
+    if (&current_boid  != &boid){
+      if(glm::distance(current_boid.GetPosition(), boid.GetPosition()) <
+         kBoidVisualRange){
+        alignment_offset_vector += current_boid.GetPosition();
+      }
+    }
+  }
+  alignment_offset_vector =
+      vec2(alignment_offset_vector.x/(float)(flock.size() - 1),
+           alignment_offset_vector.y/(float)(flock.size() - 1));
+  alignment_offset_vector =
+      vec2((alignment_offset_vector.x - boid.GetPosition().x)/4.0f,
+           (alignment_offset_vector.y - boid.GetPosition().y)/4.0f);
+  return alignment_offset_vector;
 }
 }
